493536410218668dc5a6b5f8c121fd70eb990099
eyangeric <eyangeric@gmail.com>
2024-09-10 23:21:32 -0500

    pytest for checking player's first 2 cards

--- tests/test_player.py
+++ tests/test_player.py
@@ -16,3 +16,32 @@ def test_receive_card():
     player.receive_card(card)
 
     assert player.hand[0].card_type == "A"
+
+
+def test_check_initial_hand_pair():
+    card_4 = Card(4)
+    player = Player("Card Counter")
+    player.receive_card(card_4)
+    player.receive_card(card_4)
+    player.check_initial_hand()
+    assert player.hand_type == "pair"
+
+
+def test_check_initial_hand_soft():
+    card_4 = Card(4)
+    card_A = Card("A")
+    player = Player("Card Counter")
+    player.receive_card(card_4)
+    player.receive_card(card_A)
+    player.check_initial_hand()
+    assert player.hand_type == "soft"
+
+
+def test_check_initial_hand_hard():
+    card_4 = Card(4)
+    card_5 = Card(5)
+    player = Player("Card Counter")
+    player.receive_card(card_4)
+    player.receive_card(card_5)
+    player.check_initial_hand()
+    assert player.hand_type == "hard"
