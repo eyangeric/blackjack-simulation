3afd04ff5ca07496120696e29e03ab4d504e017e
eyangeric <eyangeric@gmail.com>
2024-09-22 17:05:11 -0500

    create Hand class to store cards in a hand

---
+++ blackjack/hand.py
@@ -0,0 +1,23 @@
+from blackjack.card import Card
+
+
+class Hand:
+    def __init__(self, card: Card):
+        self.cards = [card]
+        self.sum_count = 1
+
+        if card.card_type == "A":
+            self.sums = card.value
+            self.sum_count += 1
+        else:
+            self.sums = [card.value]
+
+    def receive_card(self, card: Card):
+        self.cards.append(card)
+        if card.card_type == "A":
+            if self.sum_count > 1:
+                self.sums = [value + min(card.value) for value in self.sums]
+            else:
+                self.sums = [value + self.sums[0] for value in card.value]
+        else:
+            self.sums = [value + card.value for value in self.sums]
